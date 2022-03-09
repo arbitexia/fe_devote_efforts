f0a024d53f6990020a0274c467441d23ec4efb97
bluesky715323 <babystar19911@outlook.com>
2022-03-09 23:45:55 +0800

    Fix: Navbar Responsive

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/logo.svg'
 import '../../styles/navbar.scss'
 
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', changeWidth)
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+    }
+  }, [])
   return (
     <div className="navigation">
       <div className="container container--flex container--flex__centered">
@@ -13,23 +24,25 @@ function Navbar() {
             <img src={logo} className="nav-logo" alt="logo" />
           </Link>
         </div>
-        <ul className="menu">
-          <li>
-            <Link to="/aboutus">About Us</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/technologies">Technologies</Link>
-          </li>
-          <li>
-            <Link to="/pricing">Pricing</Link>
-          </li>
-          <li>
-            <Link to="/contactus">Contact Us</Link>
-          </li>
-        </ul>
+        {(toggleMenu || screenWidth > 1156) && (
+          <ul className="menu">
+            <li>
+              <Link to="/aboutus">About Us</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/technologies">Technologies</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/contactus">Contact Us</Link>
+            </li>
+          </ul>
+        )}
         <button
           className="dropdown"
           onClick={() => {
