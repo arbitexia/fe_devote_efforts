6a2af0ecdd76865de0094a97a3e935b807f2a749
bluesky715323 <babystar19911@outlook.com>
2022-03-12 05:51:44 +0800

    feat(trust by): first upload to create new branch

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,23 +1,8 @@
 import React from 'react'
-import logo from '../../logo.svg'
+import Clients from '../clients'
 
 function HomeContainer() {
-  return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  )
+  return <Clients />
 }
 
 export default HomeContainer
