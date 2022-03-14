f6f83d5c3cacf407b3725e7a33084101723f026e
babystar1991 <88843557+babystar1991@users.noreply.github.com>
2022-03-14 15:45:31 +0800

    Update index.tsx

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Services from '../services'
+import Clients from '../clients'
 import Hero from '../hero'
 
 function HomeContainer() {
   return (
     <main>
       <Hero />
+      <Clients />
       <Services />
     </main>
   )
